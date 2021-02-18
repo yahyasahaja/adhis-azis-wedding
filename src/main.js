bb2ce14ade16d244bc4b34b5d120e6991e0d0766
Yahya Sahaja <yahya.sahaja2@gmail.com>
2021-02-18 14:34:08 +0700

    temp: add pwa

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/styles/tailwind.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
