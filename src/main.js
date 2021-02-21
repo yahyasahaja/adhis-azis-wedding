f43f7efb5cf6b52971d6473fe55969b988bbbaf1
Yahya Sahaja <yahya.sahaja2@gmail.com>
2021-02-21 19:26:47 +0700

    fix: update address

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/styles/tailwind.css'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
